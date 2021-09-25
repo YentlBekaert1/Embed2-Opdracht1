a29175679770031a7de1667c2a73db1a92cfdf65
YentlBekaert1 <yentl.bekaert@student.vives.be>
2021-09-25 12:06:05 +0200

    change

--- Examples/Input_log/Source/main.c
+++ Examples/Input_log/Source/main.c
@@ -4,6 +4,7 @@
 #include <mysql.h>
 #include <time.h>
 
+//function to read change on input
 int buttonPress(int g){
 	int return_value;
 
@@ -24,47 +25,49 @@ int main()
         	return -1;
     	}
 
-	// Define gpio 17 as output
-	INP_GPIO(17);
-	OUT_GPIO(17);
+	// Define gpio 27 as input
 	INP_GPIO(27);
 
-	 MYSQL *con = mysql_init(NULL);
-
+	//create time variables
 	time_t current_time;
 	struct tm tm = *localtime(&current_time);
 
-  if (con == NULL) 
-  {
-      fprintf(stderr, "%s\n", mysql_error(con));
-      exit(1);
-  }
+	//add MYSQL
+	MYSQL *con = mysql_init(NULL);
+	if (con == NULL) 
+	{
+		fprintf(stderr, "%s\n", mysql_error(con));
+		exit(1);
+	}
 
-  if (mysql_real_connect(con, "localhost", "root", "root_pswd", 
-          NULL, 0, NULL, 0) == NULL) 
-  {
-      fprintf(stderr, "%s\n", mysql_error(con));
-      mysql_close(con);
-      exit(1);
-  }  
+	//connect to Mariadb
+	if (mysql_real_connect(con, "localhost", "root", "root_pswd", 
+			NULL, 0, NULL, 0) == NULL) 
+	{
+		fprintf(stderr, "%s\n", mysql_error(con));
+		mysql_close(con);
+		exit(1);
+	}  
 
- if (mysql_query(con, "USE inputs")) 
-  {
-      fprintf(stderr, "%s\n", mysql_error(con));
-      mysql_close(con);
-      exit(1);
-  }
- 
+	//USE Databese inputs
+	if (mysql_query(con, "USE inputs")) 
+	{
+		fprintf(stderr, "%s\n", mysql_error(con));
+		mysql_close(con);
+		exit(1);
+	}
 
 	int pressed = 0;
 	int old_value =0;
 	int value = 0;
 
+	//endless loop
 	while(1)
 	{
-	
+		//get te state of the input
 		int value_input_27 = buttonPress(27);
 
+		//for debouncing
 		if(value_input_27 == 1 && pressed == 0){
 			value = 1;
 			pressed = 1;
@@ -77,15 +80,18 @@ int main()
 			char number_str[1];
 			sprintf(number_str, "%d", value);
 			printf("value: %s\n", number_str);
-
+			
+			//get the time and put it in a string
 			current_time = time(NULL);
 			tm = *localtime(&current_time);
 			char time[1024];
 			sprintf(time, "%d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
+			//make the query to insert into table
 			char Query[1024];
 			sprintf(Query, "INSERT INTO subjects( time, input, state) VALUES ('%s', 27, %d)", time, value);
 
+			//execute the query
 			if (mysql_query(con, Query)) 
 			{
 					fprintf(stderr, "%s\n", mysql_error(con));
@@ -94,9 +100,8 @@ int main()
 			}
 			old_value = value;
 		}
-
 	}
-
+	//close connection
 	mysql_close(con);
 	return 0;	
 }
